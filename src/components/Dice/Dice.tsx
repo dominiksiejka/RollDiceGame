0c8ed78e67f1d73b418eb52a610ffd4b601aea7d
sieja182 <dev@dominiksiejka.pl>
2021-07-27 19:06:09 +0200

    fix the dice api url

--- src/components/Dice/Dice.tsx
+++ src/components/Dice/Dice.tsx
@@ -1,15 +1,16 @@
-import classes from './Dice.module.scss'
-interface DiceProps{
-    currentDiceVal: number;
- 
+import classes from "./Dice.module.scss";
+interface DiceProps {
+  currentDiceVal: number;
 }
-const Dice: React.FC<DiceProps> = ({ currentDiceVal}) => {
- 
-    return (
-        <section className={classes.Dice}>
-            <img src={`http://roll.diceapi.com/images/poorly-drawn/d6/${currentDiceVal}.png`} alt="choosen card" />
-        </section>
-    )
-}
+const Dice: React.FC<DiceProps> = ({ currentDiceVal }) => {
+  return (
+    <section className={classes.Dice}>
+      <img
+        src={`https://roll.diceapi.com/images/poorly-drawn/d6/${currentDiceVal}.png`}
+        alt='choosen card'
+      />
+    </section>
+  );
+};
 
 export default Dice;
